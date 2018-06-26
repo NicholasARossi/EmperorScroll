f5812eace0705699de1951667b91574a74074dc5
Nicholas A Rossi <nrossi1@uvm.edu>
2018-06-26 17:57:53 -0400

    scrollytell with map

--- JS/romanglory.js
+++ JS/romanglory.js
@@ -1,26 +1,15 @@
-let width = window.innerWidth;
-let height = window.innerHeight;
-let size_scaler=width
+
 
 d3.csv("Data/munged_data.csv", function(data) {
   console.log(data[0].reignstart);
 
 
-//Width and height
-var w = 800;
-var h = 600;
-
+let width = window.innerWidth;
+let height = window.innerHeight;
+let size_scaler=width;
 //Define map projection
 
 
-var projection = d3.geoMercator() //utiliser une projection standard pour aplatir les pôles, voir D3 projection plugin
-                       .center([ 13, 52 ]) //comment centrer la carte, longitude, latitude
-                       .translate([ w/2, h/2 ]) // centrer l'image obtenue dans le svg
-                       .scale([ w/1.5 ]); // zoom, plus la valeur est petit plus le zoom est gros
-
-//Define path generator
-var path = d3.geoPath()
-                 .projection(projection);
 
 
 
@@ -71,6 +60,25 @@ var tipMouseover = function(d,i) {
         .style("opacity", .9) // started as 0!
 
 };
+var tipMouseover2 = function(d,i) {
+let colors =["#2c7bb6","#00a6ca","#00ccbc","#90eb9d","#f9d057","#f29e2e", "#e76818","#d7191c"];
+
+    // var color = d3.interpolate("#ffffff","#FF00FF")(d.state/52);
+        var html  = data[i].name+"<br/>"+"<span style='color:" + colors[+data[i].causenums] + ";'>"+ data[i].cause  + "</span><br/>";
+                d3.select(this)
+                  .transition()
+                  .duration(300)
+                  .attr('stroke-width',3)
+    tooltip.html(html)
+        .style("left", (d3.event.pageX + 15) + "px")
+        .style("top", (d3.event.pageY - 28) + "px")
+      .transition()
+        .duration(300) // ms
+        .style("opacity", .9) // started as 0!
+
+};
+
+
 // tooltip mouseout event handler
 var tipMouseout = function(d) {
   d3.select(this)
@@ -110,12 +118,15 @@ function ticked0() {
 function ticked1() {
 
 node
+  .on("mouseover", tipMouseover)
   .transition()
   .ease(d3.easeExpOut)
   .duration(600)
 //      .delay((d, i) => 100 * i)
   .attr("opacity", "1")
   .attr("r", 10)
+  .attr("fill", function(d) { return  "#9C1C29"; })
+  .attr("stroke","none")
   .attr("cx", function(d) { return d.x; })
   .attr("cy", function(d) { return d.y; });
   };
@@ -123,6 +134,7 @@ node
 function ticked2() {
 
 node
+  .on("mouseover", tipMouseover)
   .transition()
   .ease(d3.easeExpOut)
   .duration(600)
@@ -162,6 +174,109 @@ var simulation = d3.forceSimulation(nodes)
 //
       }
 
+///// GRID Cluster
+
+let spacing = size_scaler/20;
+let rows = 10;
+let column = 10;
+
+let cluster = () =>{
+    var simulation = d3.forceSimulation(nodes)
+           .on("tick", row_cluster);
+
+      }
+function row_cluster() {
+let spacing = size_scaler/13;
+let rows = 10;
+let column = 10;
+//let colors =["#2c7bb6","#00a6ca","#00ccbc","#90eb9d","#f9d057","#f29e2e", "#e76818","#d7191c"];
+
+let colors =['#fff','#9c1c29','darkgrey','#a85350','slategrey','#ad7f7b','#a9a9a9'];
+svg2.selectAll("path")
+  .attr("opacity",0);
+node
+  .on("mouseover", tipMouseover2)
+  .transition()
+  .ease(d3.easeExpOut)
+  .duration(600)
+    .attr("stroke","none")
+    .attr("r", 10)
+    .attr("cx", (d, i) => i % column * spacing-size_scaler/2.9)
+    .attr("cy", (d, i) => Math.floor(i / 10) % rows * spacing-size_scaler/4)
+    .attr("fill", function(d,i) { return colors[+data[i].causenums]; })
+    .attr("opacity", "1")
+
+
+  };
+
+//// Plotting map
+//Width and height
+var w = size_scaler;
+var h = height;
+
+var projection = d3.geoMercator() //utiliser une projection standard pour aplatir les pôles, voir D3 projection plugin
+                       .center([ 13, 40 ]) //comment centrer la carte, longitude, latitude
+                       .translate([ 0, 0 ]) // centrer l'image obtenue dans le svg
+                       .scale([ w ]); // zoom, plus la valeur est petit plus le zoom est gros
+
+
+d3.selection.prototype.moveToBack = function() {
+    return this.each(function() {
+        var firstChild = this.parentNode.firstChild;
+        if (firstChild) {
+            this.parentNode.insertBefore(this, firstChild);
+        }
+    });
+};
+
+//Create SVG
+var svg2 =  d3.select("svg");
+
+//Define path generator
+var path = d3.geoPath()
+                 .projection(projection);
+
+//Load in GeoJSON data
+var map
+d3.json("custom.geo2.json", function(json) {
+
+    //Bind data and create one path per GeoJSON feature
+    var map=svg2.selectAll("path")
+       .data(json.features)
+       .enter()
+       .append("path")
+       .attr("d", path)
+       .attr("stroke", "black")
+       .attr("fill","#9C1C29" )
+       .attr("opacity",0);
+
+    map.moveToBack()
+});
+
+
+let mapped = () =>{
+    var simulation = d3.forceSimulation(nodes)
+           .on("tick", map_proj);
+
+      }
+
+function map_proj() {
+
+node
+  .transition()
+  .ease(d3.easeExpOut)
+  .duration(600)
+  .attr("fill","#fff")
+  .attr("stroke","#000")
+   .attr("r", 5)
+
+.attr("cx", function(d,i) {return projection([data[i].logitude, data[i].latitude])[0];})
+.attr("cy", function(d,i) {return projection([data[i].logitude, data[i].latitude])[1];});
+svg2.selectAll("path")
+  .attr("opacity",.57);
+
+  };
+//// Builing the scrollytelling entirely
 
 //waypoints scroll constructor
 function scroll(n, offset, func1, func2){
@@ -175,12 +290,12 @@ function scroll(n, offset, func1, func2){
   });
 };
 
-
-
 //triger these functions on page scroll
 new scroll('div1', '75%', ready, naught);
 new scroll('div2', '75%', gro, ready);
 new scroll('div3', '75%', ready, gro);
+new scroll('div4', '75%', cluster, ready);
+new scroll('div5', '75%', mapped, cluster);
 
 
 
